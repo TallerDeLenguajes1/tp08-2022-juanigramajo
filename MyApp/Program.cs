1c3515b4c85913c4868f48c655e685f39e9bf478
Juani <juanigramajo@hotmail.com>
2022-06-27 14:47:28 -0300

    conociendo como escribir archivos

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -17,8 +17,8 @@
 // Console.ReadKey();
 
 
-string Directorio = "/Users/juanigramajo/Desktop";
-string info = Directory.GetDirectoryRoot(Directorio);
+// string Directorio = "/Users/juanigramajo/Desktop";
+// string info = Directory.GetDirectoryRoot(Directorio);
 // System.Console.WriteLine("Info sobre la raíz del directorio: " + info);
 
 // Console.ReadKey();
@@ -44,26 +44,46 @@ string info = Directory.GetDirectoryRoot(Directorio);
 
 
 
-if (!(Directory.Exists(Directorio + "/Tempo")))
-{   
-    Directory.CreateDirectory(Directorio + "/Tempo");
-}
+// if (!(Directory.Exists(Directorio + "/Tempo")))
+// {   
+//     Directory.CreateDirectory(Directorio + "/Tempo");
+// }
 
-List<string> ListadoDeCarpetasEnRepo2 = Directory.GetDirectories(Directorio).ToList();
-System.Console.WriteLine("--------------------------------------");
-System.Console.WriteLine("Carpetas en repositorio - " + Directorio);
-foreach (string Carpeta in ListadoDeCarpetasEnRepo2)
-{
-    System.Console.WriteLine(Carpeta);
-}
+// List<string> ListadoDeCarpetasEnRepo2 = Directory.GetDirectories(Directorio).ToList();
+// System.Console.WriteLine("--------------------------------------");
+// System.Console.WriteLine("Carpetas en repositorio - " + Directorio);
+// foreach (string Carpeta in ListadoDeCarpetasEnRepo2)
+// {
+//     System.Console.WriteLine(Carpeta);
+// }
 
 
-string NuevoArchivo = Directorio + "/Tempo" + "/Prueba.txt";
-string NuevoArchivoCopia = Directorio + "/Tempo" + "/Prueba_Copia.txt";
+// string NuevoArchivo = Directorio + "/Tempo" + "/Prueba.txt";
+// string NuevoArchivoCopia = Directorio + "/Tempo" + "/Prueba_Copia.txt";
 
-if (!(File.Exists(NuevoArchivo)))
-{   
-    File.Create(NuevoArchivo);
-}
+// if (!(File.Exists(NuevoArchivo)))
+// {   
+//     File.Create(NuevoArchivo);
+// }
 
-File.Move(NuevoArchivo, NuevoArchivoCopia);
+// File.Move(NuevoArchivo, NuevoArchivoCopia);
+
+
+string RutaArchivo = "/Users/juanigramajo/Desktop/Tempo/text1.txt";
+string texto = File.ReadAllText(RutaArchivo);
+System.Console.WriteLine("Contenido: {0}", texto);
+
+
+List<string> LineasDelArchivo = File.ReadAllLines(RutaArchivo).ToList();
+
+
+// foreach (string Linea in LineasDelArchivo)
+// {
+//     System.Console.WriteLine("\t" + Linea);
+// }
+
+
+System.Console.WriteLine("\t" + LineasDelArchivo[2]);
+
+string[] MisLineas = {"Pruebs 1", "Prueba 2"};
+File.WriteAllLines(RutaArchivo, MisLineas);
